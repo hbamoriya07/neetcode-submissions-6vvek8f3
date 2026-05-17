444963078ab883dcebee8c1f4b35ba969476f29e
hbamoriya07 <74555215+hbamoriya07@users.noreply.github.com>
2026-05-18 00:29:19 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.go
@@ -0,0 +1,35 @@
+type MinStack struct {
+	stack []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack: []int{},
+		minStack: []int{},
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.minStack) == 0 || val <= this.minStack[len(this.minStack)-1] {
+		this.minStack = append(this.minStack, val)
+	}
+}
+
+func (this *MinStack) Pop() {
+	v := this.stack[len(this.stack) - 1]
+	this.stack = this.stack[:len(this.stack) - 1]
+
+	if v == this.minStack[len(this.minStack) - 1] {
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack) - 1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack) - 1]
+}
